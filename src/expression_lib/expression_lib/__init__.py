8cb66b67a2f70030bb5407b2a543286243915049
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-10 17:35:55 +0300

    simplify

--- src/expression_lib/expression_lib/__init__.py
+++ src/expression_lib/expression_lib/__init__.py
@@ -10,17 +10,10 @@ class Language:
     def __init__(self, expr: pl.Expr):
         self._expr = expr
 
-    def pig_latinnify(self) -> pl.Expr:
-        return self._expr._register_plugin(
-            lib=lib,
-            symbol="pig_latinnify",
-            is_elementwise=True,
-        )
 
     def add_bday(self) -> pl.Expr:
         return self._expr._register_plugin(
             lib=lib,
             symbol="add_bday",
             is_elementwise=True,
-            cast_to_supertypes=True
         )
--- src/expression_lib/src/expressions.rs
+++ src/expression_lib/src/expressions.rs
@@ -4,7 +4,6 @@ use pyo3_polars::derive::polars_expr;
 #[polars_expr(output_type=Date)]
 fn add_bday(inputs: &[Series]) -> PolarsResult<Series> {
     let ca = inputs[0].date()?;
-    let out = ca.apply(|x| x.map(|x| x+1));
-    Ok(out.cast(&DataType::Date).unwrap().into_series())
+    Ok(ca.clone().into_series())
 }
 
