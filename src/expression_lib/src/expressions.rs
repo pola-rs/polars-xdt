edd8fb282d6f4dd7b04baf7a8d4df3d07a29a985
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-11 14:38:25 +0300

    hypothesis tests against pandas and numpy pass!

--- src/expression_lib/src/expressions.rs
+++ src/expression_lib/src/expressions.rs
@@ -7,9 +7,20 @@ fn advance_by_days(inputs: &[Series]) -> PolarsResult<Series> {
     let n = inputs[1].i32()?.get(0).unwrap();
 
     let out = ca.apply_values(
-        |x|{
-        let w = (x / (3600 * 24 - 3)) % 7 + 1;
-        let n_days = n + (n + w) / 5 * 2;
+        |mut x|{
+        let mut weekday = (x - 4) % 7;
+
+        // If on weekend, roll backwards to previous
+        // valid date (following pandas here).
+        if weekday == 5 {
+            x -= 1;
+            weekday = 4;
+        } else if weekday == 6 {
+            x -= 2;
+            weekday = 4;
+        }
+
+        let n_days = n + (n + weekday) / 5 * 2;
         x + n_days
         }
     );
--- src/run.py
+++ src/run.py
@@ -4,12 +4,12 @@ from datetime import date, datetime
 import numpy as np
 
 df = pl.DataFrame({
-    "dates": pl.date_range(date(1, 1, 1), date(9999, 1, 1), eager=True),
+    "dates": pl.date_range(date(2000, 1, 1), date(9999, 1, 1), eager=True),
 })
 df = df.filter(pl.col('dates').dt.weekday() <6)
 
-print(df.with_columns(dates_shifted=pl.col('dates').bdt.advance_by_days(n=15))[20:28])
-print(df.with_columns(dates_shifted=pl.Series(np.busday_offset(df['dates'], 15)))[20:28]) 
+print(df.head().with_columns(dates_shifted=pl.col('dates').bdt.advance_by_days(n=3))[:5])
+print(df.head().with_columns(dates_shifted=pl.Series(np.busday_offset(df.head()['dates'], 3)))[:5]) 
 
 import pandas as pd
 dfpd = df.to_pandas()
