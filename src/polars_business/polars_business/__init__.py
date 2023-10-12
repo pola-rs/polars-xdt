2b0e56a84cc6d3e064773c1f4cb9daf60f2d5d4a
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-12 15:23:11 +0300

    fmt python

--- src/polars_business/polars_business/__init__.py
+++ src/polars_business/polars_business/__init__.py
@@ -12,11 +12,7 @@ class BusinessDayTools:
     def __init__(self, expr: pl.Expr):
         self._expr = expr.cast(pl.Int32)
 
-
-    def advance_n_days(self,
-                       n,
-                       holidays = None
-                       ) -> pl.Expr:
+    def advance_n_days(self, n, holidays=None) -> pl.Expr:
         # if not (isinstance(n, int) and n > 0):
         #     raise ValueError("only positive integers are currently supported for `n`")
         if holidays is None:
@@ -24,18 +20,14 @@ class BusinessDayTools:
                 lib=lib,
                 symbol="advance_n_days",
                 is_elementwise=True,
-                args = [
+                args=[
                     n,
-                    ],
+                ],
             )
         else:
             return self._expr._register_plugin(
                 lib=lib,
                 symbol="advance_n_days",
                 is_elementwise=True,
-                args = [
-                    n,
-                    pl.Series([list(set(holidays))]).cast(pl.List(pl.Int32))
-                    ],
+                args=[n, pl.Series([list(set(holidays))]).cast(pl.List(pl.Int32))],
             )
-
--- src/run.py
+++ src/run.py
@@ -3,6 +3,13 @@ from polars_business import *
 from datetime import date, datetime
 import numpy as np
 import holidays
-hols = holidays.country_holidays('UK', years=[2020])
 
-print(df.with_columns(pl.col('ts').business.advance_n_days(5, holidays=[date(2000, 1, 1), date(2000, 5, 1)])))
+hols = holidays.country_holidays("UK", years=[2020])
+
+print(
+    df.with_columns(
+        pl.col("ts").business.advance_n_days(
+            5, holidays=[date(2000, 1, 1), date(2000, 5, 1)]
+        )
+    )
+)
