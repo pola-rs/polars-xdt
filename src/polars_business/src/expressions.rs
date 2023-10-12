cc21e38033bf16fe72620ac148dc94e3d4cf2f99
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-12 15:15:34 +0300

    tmp

--- src/polars_business/src/expressions.rs
+++ src/polars_business/src/expressions.rs
@@ -14,6 +14,44 @@ fn calculate_n_days_without_holidays(n: i32, x_weekday: i32) -> i32 {
     }
 }
 
+fn reduce_vec(vec: &Vec<Option<i32>>, x: i32, n_days: i32) -> Vec<Option<i32>> {
+    // Each day we skip may be a holiday, and so require skipping an additional day.
+    // n_days*2 is an upper-bound.
+    if n_days > 0 {
+        vec.clone().into_iter().filter(|t| t.map(|t| t >= x && t <= x + n_days*2).unwrap_or(false)).collect()
+    } else {
+        vec.clone().into_iter().filter(|t| t.map(|t| t <= x && t >= x + n_days*2).unwrap_or(false)).collect()
+    }
+}
+
+fn increment_n_days(x: i32) -> i32 {
+    if x > 0 {
+        x+1
+    } else {
+        x-1
+    }
+}
+
+fn roll(n_days: i32, weekday_res: i32) -> i32 {
+    if n_days > 0 {
+        if weekday_res == 5 {
+            n_days + 2
+        } else if weekday_res == 6 {
+            n_days + 1
+        } else{
+            n_days
+        }
+    } else {
+        if weekday_res == 5 {
+            n_days - 1
+        } else if weekday_res == 6 {
+            n_days - 2
+        } else {
+            n_days
+        }
+    }
+}
+
 fn calculate_n_days(x: i32, n: i32, vec: &Vec<Option<i32>>) -> PolarsResult<i32> {
     let x_weekday = weekday(x);
 
@@ -26,28 +64,31 @@ fn calculate_n_days(x: i32, n: i32, vec: &Vec<Option<i32>>) -> PolarsResult<i32>
     let mut n_days = calculate_n_days_without_holidays(n, x_weekday);
 
     if vec.len() > 0 {
-        let mut myvec: Vec<Option<i32>> = vec.clone().into_iter().filter(|t| t.map(|t| t >= x && t <= x + n_days*2).unwrap_or(false)).collect();
+        let mut myvec: Vec<Option<i32>> = reduce_vec(vec, x, n_days);
         let mut count_hols = count_holidays(x, x+n_days, &mut myvec);
         while count_hols > 0 {
             for _ in 0..count_hols {
-                n_days += 1;
-                let res = x + n_days;
-                let weekday_res = weekday(res);
-                if weekday_res == 5 {
-                    n_days += 2;
-                } else if weekday_res == 6 {
-                    n_days += 1;
-                };
+                n_days = increment_n_days(n_days);
+                let weekday_res = weekday(x+n_days);
+                n_days = roll(n_days, weekday_res);
             }
             count_hols = count_holidays(x, x+n_days, &mut myvec);
         }
     };
-
-
     Ok(x + n_days)
 }
 
+fn condition(x: i32, start: i32, end: i32) -> bool {
+    if end > start {
+        x >= start && x <= end
+    } else {
+        x <= start && x >= end
+    }
+}
+
 fn count_holidays(start: i32, end: i32, holidays: &mut Vec<Option<i32>>) -> i32 {
+    // Count how many holidays are between 'start' and 'end', and remove
+    // them from 'holidays'.
     let mut counter = 0;  // Initialize the counter to 0
 
     // Iterate over the indices of the holidays vector in reverse order
@@ -56,7 +97,7 @@ fn count_holidays(start: i32, end: i32, holidays: &mut Vec<Option<i32>>) -> i32 
     while index >= 0 {
         // Check if the holiday is within the specified range and is Some
         if let Some(value) = holidays[index as usize] {
-            if value >= start && value <= end {
+            if condition(value, start, end) {
                 // If the holiday is within the range, increment the counter
                 counter += 1;
                 // Remove the holiday from the vector
--- src/run.py
+++ src/run.py
@@ -2,23 +2,7 @@ import polars as pl
 from polars_business import *
 from datetime import date, datetime
 import numpy as np
+import holidays
+hols = holidays.country_holidays('UK', years=[2020])
 
-# df = pl.DataFrame({
-#     "dates": pl.date_range(date(2000, 12, 26), date(2000, 12, 30), eager=True),
-# })
-# df = df.filter(pl.col('dates').dt.weekday() <6)
-
-# print(df.with_columns(dates_shifted=pl.col('dates').business.advance_n_days(
-#     n=3,
-#     holidays=[date(2000, 12, 25)]
-#     )))
-# print(df.with_columns(dates_shifted=pl.Series(np.busday_offset(df['dates'], 3,
-#                                                                       holidays=[date(2000, 12, 25)]
-#                                                                       )))) 
-
-# E           date=datetime.date(2000, 4, 24),
-# E           n=5,
-# E           holidays=[datetime.date(2000, 1, 1), datetime.date(2000, 5, 1)],
-
-df = pl.DataFrame({'ts': date(2000, 4, 24)})
 print(df.with_columns(pl.col('ts').business.advance_n_days(5, holidays=[date(2000, 1, 1), date(2000, 5, 1)])))
