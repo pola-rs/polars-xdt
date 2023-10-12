4405d1620f75fee0b28cea22ddc0060e40c70afb
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-12 15:18:10 +0300

    cargo fmt --all

--- src/polars_business/src/expressions.rs
+++ src/polars_business/src/expressions.rs
@@ -10,7 +10,7 @@ fn calculate_n_days_without_holidays(n: i32, x_weekday: i32) -> i32 {
     if n >= 0 {
         n + (n + x_weekday) / 5 * 2
     } else {
-        -(-n + (-n + 4-x_weekday) / 5 * 2)
+        -(-n + (-n + 4 - x_weekday) / 5 * 2)
     }
 }
 
@@ -18,17 +18,23 @@ fn reduce_vec(vec: &Vec<Option<i32>>, x: i32, n_days: i32) -> Vec<Option<i32>> {
     // Each day we skip may be a holiday, and so require skipping an additional day.
     // n_days*2 is an upper-bound.
     if n_days > 0 {
-        vec.clone().into_iter().filter(|t| t.map(|t| t >= x && t <= x + n_days*2).unwrap_or(false)).collect()
+        vec.clone()
+            .into_iter()
+            .filter(|t| t.map(|t| t >= x && t <= x + n_days * 2).unwrap_or(false))
+            .collect()
     } else {
-        vec.clone().into_iter().filter(|t| t.map(|t| t <= x && t >= x + n_days*2).unwrap_or(false)).collect()
+        vec.clone()
+            .into_iter()
+            .filter(|t| t.map(|t| t <= x && t >= x + n_days * 2).unwrap_or(false))
+            .collect()
     }
 }
 
 fn increment_n_days(x: i32) -> i32 {
     if x > 0 {
-        x+1
+        x + 1
     } else {
-        x-1
+        x - 1
     }
 }
 
@@ -38,7 +44,7 @@ fn roll(n_days: i32, weekday_res: i32) -> i32 {
             n_days + 2
         } else if weekday_res == 6 {
             n_days + 1
-        } else{
+        } else {
             n_days
         }
     } else {
@@ -65,14 +71,14 @@ fn calculate_n_days(x: i32, n: i32, vec: &Vec<Option<i32>>) -> PolarsResult<i32>
 
     if vec.len() > 0 {
         let mut myvec: Vec<Option<i32>> = reduce_vec(vec, x, n_days);
-        let mut count_hols = count_holidays(x, x+n_days, &mut myvec);
+        let mut count_hols = count_holidays(x, x + n_days, &mut myvec);
         while count_hols > 0 {
             for _ in 0..count_hols {
                 n_days = increment_n_days(n_days);
-                let weekday_res = weekday(x+n_days);
+                let weekday_res = weekday(x + n_days);
                 n_days = roll(n_days, weekday_res);
             }
-            count_hols = count_holidays(x, x+n_days, &mut myvec);
+            count_hols = count_holidays(x, x + n_days, &mut myvec);
         }
     };
     Ok(x + n_days)
@@ -89,7 +95,7 @@ fn condition(x: i32, start: i32, end: i32) -> bool {
 fn count_holidays(start: i32, end: i32, holidays: &mut Vec<Option<i32>>) -> i32 {
     // Count how many holidays are between 'start' and 'end', and remove
     // them from 'holidays'.
-    let mut counter = 0;  // Initialize the counter to 0
+    let mut counter = 0; // Initialize the counter to 0
 
     // Iterate over the indices of the holidays vector in reverse order
     // so that we can remove elements without causing issues with indexing
@@ -125,24 +131,21 @@ fn advance_n_days(inputs: &[Series]) -> PolarsResult<Series> {
     } else {
         Vec::new()
     };
-    let vec: Vec<_> = vec.into_iter().filter(|x| x.map(|x| weekday(x) < 5).unwrap_or(false)).collect();
+    let vec: Vec<_> = vec
+        .into_iter()
+        .filter(|x| x.map(|x| weekday(x) < 5).unwrap_or(false))
+        .collect();
 
     let out = match n.len() {
         1 => {
             if let Some(n) = n.get(0) {
-                ca.try_apply(
-                    |x|{
-                    calculate_n_days(x, n, &vec)
-                    }
-                )
+                ca.try_apply(|x| calculate_n_days(x, n, &vec))
             } else {
                 Ok(Int32Chunked::full_null(ca.name(), ca.len()))
             }
         }
         _ => try_binary_elementwise(ca, n, |opt_s, opt_n| match (opt_s, opt_n) {
-            (Some(s), Some(n)) => {
-                calculate_n_days(s, n, &vec).map(Some)
-            }
+            (Some(s), Some(n)) => calculate_n_days(s, n, &vec).map(Some),
             _ => Ok(None),
         }),
     };
