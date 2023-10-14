5a61cd4d1d99e90b44dd32809c1142600008dc88
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-14 12:47:06 +0300

    ergonomics

--- tests/test_business_offsets.py
+++ tests/test_business_offsets.py
@@ -9,6 +9,8 @@ import polars as pl
 import polars_business  # noqa: F401
 
 
+reverse_mapping = {value: key for key, value in polars_business.mapping.items()}
+
 @given(
     date=st.dates(min_value=dt.date(1000, 1, 1), max_value=dt.date(9999, 12, 31)),
     n=st.integers(min_value=-30, max_value=30),
@@ -65,46 +67,34 @@ def test_against_np_busday_offset_with_holidays(date: dt.date, n: int, holidays:
 @given(
     date=st.dates(min_value=dt.date(2000, 1, 1), max_value=dt.date(2000, 12, 31)),
     n=st.integers(min_value=-30, max_value=30),
-    weekend = st.lists(st.sampled_from([0,1,2,3,4,5,6]), min_size=0, max_size=7)
+    weekend = st.lists(st.sampled_from(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']), min_size=0, max_size=7),
 )
 def test_against_np_busday_offset_with_weekends(date: dt.date, n: int, weekend: list[dt.date]) -> None:
-    assume(date.weekday() < 5)
-    assume(date.weekday() not in weekend)  # TODO: remove once unwrap is removed
-    try:
-        result = pl.DataFrame({'ts': [date]}).select(pl.col('ts').business.advance_n_days(
-            n=n,
-            weekend=weekend,
-            ))['ts'].item()
-    except pl.PolarsPanicError as exc:
-        assert date.weekend() in weekend
-        assert 'cannot advance' in str(exc)
-        reject()
+    assume(reverse_mapping[date.weekday()] not in weekend)  # TODO: remove once unwrap is removed
+    result = pl.DataFrame({'ts': [date]}).select(pl.col('ts').business.advance_n_days(
+        n=n,
+        weekend=weekend,
+        ))['ts'].item()
 
-    weekmask = [0 if i in weekend else 1 for i in range(7)]
+    weekmask = [0 if reverse_mapping[i] in weekend else 1 for i in range(7)]
     expected = np.busday_offset(date, n, weekmask=weekmask)
     assert np.datetime64(result) == expected
 
 @given(
     date=st.dates(min_value=dt.date(2000, 1, 1), max_value=dt.date(2000, 12, 31)),
     n=st.integers(min_value=-30, max_value=30),
-    weekend = st.lists(st.sampled_from([0,1,2,3,4,5,6]), min_size=0, max_size=7),
+    weekend = st.lists(st.sampled_from(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']), min_size=0, max_size=7),
     holidays = st.lists(st.dates(min_value=dt.date(2000, 1, 1), max_value=dt.date(2000, 12, 31)), min_size=1, max_size=300)
 )
 def test_against_np_busday_offset_with_weekends_and_holidays(date: dt.date, n: int, weekend: list[int], holidays: list[dt.date]) -> None:
-    assume(date.weekday() < 5)
-    assume(date.weekday() not in weekend)  # TODO: remove once unwrap is removed
+    assume(reverse_mapping[date.weekday()] not in weekend)  # TODO: remove once unwrap is removed
     assume(date not in holidays)  # TODO: remove once unwrap is removed
-    try:
-        result = pl.DataFrame({'ts': [date]}).select(pl.col('ts').business.advance_n_days(
-            n=n,
-            weekend=weekend,
-            holidays=holidays,
-            ))['ts'].item()
-    except pl.PolarsPanicError as exc:
-        assert date.weekend() in weekend
-        assert 'cannot advance' in str(exc)
-        reject()
+    result = pl.DataFrame({'ts': [date]}).select(pl.col('ts').business.advance_n_days(
+        n=n,
+        weekend=weekend,
+        holidays=holidays,
+        ))['ts'].item()
 
-    weekmask = [0 if i in weekend else 1 for i in range(7)]
+    weekmask = [0 if reverse_mapping[i] in weekend else 1 for i in range(7)]
     expected = np.busday_offset(date, n, weekmask=weekmask, holidays=holidays)
     assert np.datetime64(result) == expected
