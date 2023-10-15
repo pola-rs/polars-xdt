949bbf5edde5426ba77ec6641fc2070bcc378c60
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-15 19:14:51 +0300

    update perf

--- src/perf.py
+++ src/perf.py
@@ -2,8 +2,8 @@
 import timeit
 import numpy as np
 
-BENCHMARKS = [1, 2, 3]
-# BENCHMARKS = [3]
+# BENCHMARKS = [1, 2, 3]
+BENCHMARKS = [4]
 
 # BENCHMARK 1: NO HOLIDAYS INVOLVED
 
@@ -108,3 +108,36 @@ df_pd = pd.DataFrame({
 if 3 in BENCHMARKS:
     print('Polars-business: ', time_it("result_pl = df.select(pl.col('ts').business.advance_n_days(n=17, weekend=weekend))"))
     print('NumPy: ', time_it("result_np = np.busday_offset(input_dates, 17, weekmask='1111001')"))
+
+
+# BENCHMARK 4: WITH weekends and holidays
+
+setup = """
+import polars as pl
+import polars_business
+from datetime import date
+import numpy as np
+import pandas as pd
+import holidays
+import warnings
+
+uk_holidays = list(holidays.country_holidays('UK', years=[2020, 2021, 2022, 2023]))
+
+dates = pl.date_range(date(2020, 1, 1), date(2024, 1, 1), closed='left', eager=True)
+weekend = ['Fri', 'Sat']
+dates = dates.filter((~dates.dt.weekday().is_in([5, 6])) & (~dates.is_in(uk_holidays)))
+size = 10_000_000
+input_dates = np.random.choice(dates, size)
+
+df = pl.DataFrame({
+    'ts': input_dates,
+})
+
+df_pd = pd.DataFrame({
+    'ts': input_dates,
+})
+"""
+
+if 4 in BENCHMARKS:
+    print('Polars-business: ', time_it("result_pl = df.select(pl.col('ts').business.advance_n_days(n=17, weekend=weekend, holidays=uk_holidays))"))
+    print('NumPy: ', time_it("result_np = np.busday_offset(input_dates, 17, weekmask='1111001', holidays=uk_holidays)"))
