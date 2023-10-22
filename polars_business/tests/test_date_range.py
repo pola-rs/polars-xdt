c2c979153e9e3b358bd54c69a8e886884a434d41
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-22 14:48:18 +0100

    add tests for date range

--- polars_business/tests/test_date_range.py
+++ polars_business/tests/test_date_range.py
@@ -18,3 +18,13 @@ def test_expr() -> None:
 def test_invalid() -> None:
     with pytest.raises(ValueError):
         plb.date_range(date(2023, 1, 1), date(2023, 1, 10), '1bd1h')
+
+def test_eager_custom_weekend() -> None:
+    result = plb.date_range(date(2023, 1, 1), date(2023, 1, 10), eager=True, weekend=['Fri', 'Sat'])
+    expected = pl.Series('date', [date(2023, 1, 1), date(2023, 1, 2), date(2023, 1, 3), date(2023, 1, 4), date(2023, 1, 5), date(2023, 1, 8), date(2023, 1, 9), date(2023, 1, 10)])
+    assert_series_equal(result, expected)
+
+def test_eager_custom_holiday() -> None:
+    result = plb.date_range(date(2023, 1, 1), date(2023, 1, 10), eager=True, weekend=['Fri', 'Sat'], holidays=[date(2023, 1, 2)])
+    expected = pl.Series('date', [date(2023, 1, 1), date(2023, 1, 3), date(2023, 1, 4), date(2023, 1, 5), date(2023, 1, 8), date(2023, 1, 9), date(2023, 1, 10)])
+    assert_series_equal(result, expected)
