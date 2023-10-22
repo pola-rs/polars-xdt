a59eaa1e6b50aec02f8c79efd461265ae56b7b0b
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-22 14:12:59 +0100

    factor date_range out

--- polars_business/polars_business/polars_business/__init__.py
+++ polars_business/polars_business/polars_business/__init__.py
@@ -1,12 +1,12 @@
 import polars as pl
-from polars.type_aliases import IntoExpr
 from polars.utils.udfs import _get_shared_lib_location
-from datetime import date, datetime, timedelta
 import re
-from polars.functions.col import _create_col
+from datetime import date
 
-from polars.type_aliases import IntoExprColumn, ClosedInterval, TimeUnit, PolarsDataType
-from typing import Sequence, Literal, overload, cast, Iterable, Protocol
+from polars_business.ranges import date_range
+
+from polars.type_aliases import PolarsDataType
+from typing import Sequence, cast, Iterable, Protocol
 
 lib = _get_shared_lib_location(__file__)
 
@@ -105,106 +105,12 @@ class ExprBusinessDateTimeNamespace:
         return result.dt.offset_by(by)
 
 
-@overload
-def date_range(
-    start: date | datetime | IntoExprColumn,
-    end: date | datetime | IntoExprColumn,
-    interval: str | timedelta = "1d",
-    *,
-    closed: ClosedInterval = ...,
-    time_unit: TimeUnit | None = ...,
-    time_zone: str | None = ...,
-    eager: Literal[False] = ...,
-    weekend: Sequence[str] = ...,
-    holidays: Sequence[date] | None = ...,
-) -> pl.Expr:
-    ...
-
-
-@overload
-def date_range(
-    start: date | datetime | IntoExprColumn,
-    end: date | datetime | IntoExprColumn,
-    interval: str | timedelta = "1d",
-    *,
-    closed: ClosedInterval = ...,
-    time_unit: TimeUnit | None = ...,
-    time_zone: str | None = ...,
-    eager: Literal[True],
-    weekend: Sequence[str] = ...,
-    holidays: Sequence[date] | None = ...,
-) -> pl.Series:
-    ...
-
-
-@overload
-def date_range(
-    start: date | datetime | IntoExprColumn,
-    end: date | datetime | IntoExprColumn,
-    interval: str | timedelta = "1d",
-    *,
-    closed: ClosedInterval = ...,
-    time_unit: TimeUnit | None = ...,
-    time_zone: str | None = ...,
-    eager: bool = ...,
-    weekend: Sequence[str] = ...,
-    holidays: Sequence[date] | None = ...,
-) -> pl.Series | pl.Expr:
-    ...
-
-
-def date_range(
-    start: date | datetime | IntoExprColumn,
-    end: date | datetime | IntoExprColumn,
-    interval: str | timedelta = "1bd",
-    *,
-    closed: ClosedInterval = "both",
-    time_unit: TimeUnit | None = None,
-    time_zone: str | None = None,
-    eager: bool = False,
-    weekend: Sequence[str] = ("Sat", "Sun"),
-    holidays: Sequence[date] | None = None,
-) -> pl.Series | pl.Expr:
-    """
-    Utility function for filtering out weekends and holidays from a date range.
-    """
-    if weekend == ("Sat", "Sun"):
-        weekend_int = [6, 7]
-    else:
-        weekend_int = sorted({mapping[name] for name in weekend})
-    if holidays is None:
-        holidays = []
-
-    if not re.match(r'^-?\d+bd$', interval):
-        raise ValueError(
-            "Only intervals of the form 'nbd' (where n is an integer) are supported."
-        )
-    interval = interval.replace('bd', 'd')
-
-    expr = pl.date_range(
-        start,
-        end,
-        interval,
-        closed=closed,
-        time_unit=time_unit,
-        time_zone=time_zone,
-        eager=False,
-    )
-    expr = expr.filter(~expr.is_in(holidays))
-    expr = expr.filter(~expr.dt.weekday().is_in(weekend_int))
-    if eager:
-        df = pl.select(expr)
-        return df[df.columns[0]]
-    return expr
-
-
 class BExpr(pl.Expr):
     @property
     def bdt(self) -> ExprBusinessDateTimeNamespace:
         return ExprBusinessDateTimeNamespace(self)
 
 
-# check polars.functions.col.Column
 class BColumn(Protocol):
     def __call__(
         self,
---
+++ polars_business/polars_business/polars_business/ranges.py
@@ -0,0 +1,101 @@
+from typing import overload
+import re
+
+from datetime import datetime, date, timedelta
+from typing import Literal, Sequence
+import polars as pl
+from polars.type_aliases import IntoExprColumn, ClosedInterval, TimeUnit
+
+mapping = {"Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
+
+@overload
+def date_range(
+    start: date | datetime | IntoExprColumn,
+    end: date | datetime | IntoExprColumn,
+    interval: str | timedelta = "1d",
+    *,
+    closed: ClosedInterval = ...,
+    time_unit: TimeUnit | None = ...,
+    time_zone: str | None = ...,
+    eager: Literal[False] = ...,
+    weekend: Sequence[str] = ...,
+    holidays: Sequence[date] | None = ...,
+) -> pl.Expr:
+    ...
+
+
+@overload
+def date_range(
+    start: date | datetime | IntoExprColumn,
+    end: date | datetime | IntoExprColumn,
+    interval: str | timedelta = "1d",
+    *,
+    closed: ClosedInterval = ...,
+    time_unit: TimeUnit | None = ...,
+    time_zone: str | None = ...,
+    eager: Literal[True],
+    weekend: Sequence[str] = ...,
+    holidays: Sequence[date] | None = ...,
+) -> pl.Series:
+    ...
+
+
+@overload
+def date_range(
+    start: date | datetime | IntoExprColumn,
+    end: date | datetime | IntoExprColumn,
+    interval: str | timedelta = "1d",
+    *,
+    closed: ClosedInterval = ...,
+    time_unit: TimeUnit | None = ...,
+    time_zone: str | None = ...,
+    eager: bool = ...,
+    weekend: Sequence[str] = ...,
+    holidays: Sequence[date] | None = ...,
+) -> pl.Series | pl.Expr:
+    ...
+
+
+def date_range(
+    start: date | datetime | IntoExprColumn,
+    end: date | datetime | IntoExprColumn,
+    interval: str | timedelta = "1bd",
+    *,
+    closed: ClosedInterval = "both",
+    time_unit: TimeUnit | None = None,
+    time_zone: str | None = None,
+    eager: bool = False,
+    weekend: Sequence[str] = ("Sat", "Sun"),
+    holidays: Sequence[date] | None = None,
+) -> pl.Series | pl.Expr:
+    """
+    Utility function for filtering out weekends and holidays from a date range.
+    """
+    if weekend == ("Sat", "Sun"):
+        weekend_int = [6, 7]
+    else:
+        weekend_int = sorted({mapping[name] for name in weekend})
+    if holidays is None:
+        holidays = []
+
+    if not re.match(r'^-?\d+bd$', interval):
+        raise ValueError(
+            "Only intervals of the form 'nbd' (where n is an integer) are supported."
+        )
+    interval = interval.replace('bd', 'd')
+
+    expr = pl.date_range(
+        start,
+        end,
+        interval,
+        closed=closed,
+        time_unit=time_unit,
+        time_zone=time_zone,
+        eager=False,
+    )
+    expr = expr.filter(~expr.is_in(holidays))
+    expr = expr.filter(~expr.dt.weekday().is_in(weekend_int))
+    if eager:
+        df = pl.select(expr)
+        return df[df.columns[0]]
+    return expr
