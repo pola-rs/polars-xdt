457725eeb87450d1172c24336a8fb7b9e25a5cc6
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-10-28 12:08:05 +0100

    Bump version to 0.2.1

--- polars_business/polars_business/polars_business/__init__.py
+++ polars_business/polars_business/polars_business/__init__.py
@@ -10,7 +10,7 @@ from typing import Sequence, cast, Iterable, Protocol
 
 lib = _get_shared_lib_location(__file__)
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 mapping = {"Mon": 1, "Tue": 2, "Wed": 3, "Thu": 4, "Fri": 5, "Sat": 6, "Sun": 7}
 reverse_mapping = {value: key for key, value in mapping.items()}
