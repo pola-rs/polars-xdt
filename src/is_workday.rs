c76e11b9c7d5c7a9155c837acffff83c7f1e3c15
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-05 16:46:04 +0000

    install reqs

--- src/is_workday.rs
+++ src/is_workday.rs
@@ -1,5 +1,6 @@
 use crate::business_days::weekday;
 use polars::prelude::*;
+use pyo3_polars::export::polars_core::utils::{arrow::array::BooleanArray, CustomIterTools};
 
 pub(crate) fn impl_is_workday(
     dates: &Series,
@@ -9,22 +10,24 @@ pub(crate) fn impl_is_workday(
     let out = match dates.dtype() {
         DataType::Date => {
             let dates = dates.date()?;
-            dates.apply(|x_date| {
-                x_date.map(|date| {
-                    let day_of_week = weekday(date) as usize;
-                    match unsafe { *weekmask.get_unchecked(day_of_week - 1) }
-                        & (!holidays.contains(&date))
-                    {
-                        true => 1,
-                        false => 0,
-                    }
-                })
-            })
+            dates.downcast_iter().map(
+                |arr| -> BooleanArray {
+                    arr.into_iter().map(|date| {
+                        if let Some(date) = date {
+                            let day_of_week = weekday(*date) as usize;
+                            Some(unsafe { *weekmask.get_unchecked(day_of_week - 1) }
+                                & (!holidays.contains(&date)))
+                        } else {
+                            None
+                        }
+                    }).collect_trusted()
+                }
+            )
         }
         _ => {
             polars_bail!(InvalidOperation: "polars_xdt is_workday currently only works on Date type. \
             For now, please cast to Date first.")
         }
     };
-    Ok(out.cast(&DataType::Boolean)?.into_series())
+    Ok(BooleanChunked::from_chunk_iter(dates.name(), out).into_series())
 }
