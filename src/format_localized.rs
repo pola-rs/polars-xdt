797c2c82866781c7ed10b59555f390416387adf7
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-06 15:51:04 +0000

    support timezones too!

--- src/format_localized.rs
+++ src/format_localized.rs
@@ -1,8 +1,31 @@
-use chrono;
+use chrono::format::StrftimeItems;
+use chrono::{self, format::DelayedFormat};
 use chrono::TimeZone;
 use polars::prelude::*;
 use polars_arrow::array::{MutableArray, MutableUtf8Array, Utf8Array};
 use std::fmt::Write;
+use std::str::FromStr;
+
+fn timestamp_ms_to_datetime(timestamp: i64) -> chrono::NaiveDateTime {
+    // Just unwrap because we know that the timestamp is from a valid datetime
+    chrono::NaiveDateTime::from_timestamp_opt(
+        timestamp / 1_000,
+        (timestamp % 1_000 * 1_000_000) as u32,
+    ).unwrap()
+}
+fn timestamp_us_to_datetime(timestamp: i64) -> chrono::NaiveDateTime {
+    chrono::NaiveDateTime::from_timestamp_opt(
+        timestamp / 1_000_000,
+        (timestamp % 1_000_000 * 1_000) as u32,
+    ).unwrap()
+}
+fn timestamp_ns_to_datetime(timestamp: i64) -> chrono::NaiveDateTime {
+    chrono::NaiveDateTime::from_timestamp_opt(
+        timestamp / 1_000_000_000,
+        (timestamp % 1_000_000_000) as u32,
+    ).unwrap()
+}
+
 
 pub(crate) fn impl_format_localized(
     s: &Series,
@@ -19,29 +42,20 @@ pub(crate) fn impl_format_localized(
     let name = ca.name();
 
     match s.dtype() {
-        DataType::Datetime(time_unit, _time_zone) => {
-            fn timestamp_to_datetime(
-                timestamp: i64,
-                time_unit: &TimeUnit,
-            ) -> chrono::NaiveDateTime {
-                match time_unit {
-                    TimeUnit::Milliseconds => chrono::NaiveDateTime::from_timestamp_opt(
-                        timestamp / 1_000,
-                        (timestamp % 1_000 * 1_000_000) as u32,
-                    )
-                    .unwrap(),
-                    TimeUnit::Microseconds => chrono::NaiveDateTime::from_timestamp_opt(
-                        timestamp / 1_000_000,
-                        (timestamp % 1_000_000 * 1_000) as u32,
-                    )
-                    .unwrap(),
-                    TimeUnit::Nanoseconds => chrono::NaiveDateTime::from_timestamp_opt(
-                        timestamp / 1_000_000_000,
-                        (timestamp % 1_000_000_000) as u32,
-                    )
-                    .unwrap(),
-                }
+        DataType::Datetime(time_unit, time_zone) => {
+            let timestamp_to_datetime = match time_unit {
+                TimeUnit::Nanoseconds => timestamp_ns_to_datetime,
+                TimeUnit::Microseconds => timestamp_us_to_datetime,
+                TimeUnit::Milliseconds => timestamp_ms_to_datetime,
             };
+            let tz = match time_zone {
+                None => chrono_tz::UTC,
+                Some(tz) => chrono_tz::Tz::from_str(tz).unwrap()
+            };
+            fn format_ndt<'a>(ndt: chrono::NaiveDateTime, format: &'a str, locale: chrono::prelude::Locale, tz: chrono_tz::Tz) -> DelayedFormat<StrftimeItems<'a>>{
+                let dt = tz.from_utc_datetime(&ndt);
+                dt.format_localized(&format, locale)
+            }
             let mut ca: StringChunked = ca.apply_kernel_cast(&|arr| {
                 let mut buf = String::new();
                 let mut mutarr =
@@ -52,9 +66,8 @@ pub(crate) fn impl_format_localized(
                         None => mutarr.push_null(),
                         Some(timestamp) => {
                             buf.clear();
-                            let ndt = timestamp_to_datetime(*timestamp, time_unit);
-                            let dt = chrono::Utc.from_utc_datetime(&ndt);
-                            let fmted = dt.format_localized(&format, locale);
+                            let ndt = timestamp_to_datetime(*timestamp);
+                            let fmted = format_ndt(ndt, &format, locale, tz);
                             write!(buf, "{fmted}").unwrap();
                             mutarr.push(Some(&buf))
                         }
