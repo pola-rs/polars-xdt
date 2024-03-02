504de324176a567b057334d7385d99547e693af2
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-02 16:49:15 +0000

    wip

--- src/ewm_time.rs
+++ src/ewm_time.rs
@@ -7,22 +7,24 @@ pub(crate) fn impl_ewm_time(
 ) -> Float64Chunked {
     let mut old_wt = 1.;
     let mut prev_time: i64 = 0;
-    let mut weighted: f64 = 0.;
+    let mut prev_result: f64 = 0.;
     values.iter().zip(times.iter()).enumerate().map(|(i, (value, time))| {
         match (time, value) {
             (Some(time), Some(value)) => {
+                let mut result: f64;
                 if i == 0 {
-                    weighted = value;
+                    result = value;
                 }
                 else {
                     let delta = (time - prev_time) as f64 / halflife as f64;
                     old_wt *= (0.5 as f64).powf(delta as f64);
-                    weighted = old_wt * weighted + value;
-                    weighted /= old_wt + 1.;
+                    result = old_wt * prev_result + value;
+                    result /= old_wt + 1.;
                     old_wt += 1.;
                 }
                 prev_time = time;
-                Some(weighted)
+                prev_result = result;
+                Some(value)
             }
             _ => None,
         }
